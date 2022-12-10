5c36d53238d46c308509129bb6e2b109a02a074d
Mikalai Daronin <me@mikalaidaronin.info>
2022-12-10 12:49:12 +0300

    Fix incorrect context used in `AutofillManager` on Android.

--- PasswordAutofillSample.Android/AndroidAutofillManager.cs
+++ PasswordAutofillSample.Android/AndroidAutofillManager.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿using Android.OS;
 using Android.Views.Autofill;
-using Android.App;
-using Android.OS;
+using AndroidX.AppCompat.App;
 using PasswordAutofillSample.Droid;
 
 [assembly: Xamarin.Forms.Dependency(typeof(AndroidAutofillManager))]
@@ -11,9 +10,10 @@ namespace PasswordAutofillSample.Droid
     {
         public void Commit()
         {
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O
+                && Xamarin.Essentials.Platform.CurrentActivity is AppCompatActivity activity)
             {
-                var manager = (AutofillManager)Application.Context.GetSystemService(Java.Lang.Class.FromType(typeof(AutofillManager)));
+                var manager = (AutofillManager)activity.GetSystemService(Java.Lang.Class.FromType(typeof(AutofillManager)));
                 manager.Commit();
             }
         }
